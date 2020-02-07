2348bcb84c0786917ab74fe121eb7b45f43e7d88
Daniel Kowalski <danielkow19@gmail.com>
2020-02-07 09:39:23 -0500

    Filled out/completed the Collectable GameObject class

--- First MonoGame Game/Collectable.cs
+++ First MonoGame Game/Collectable.cs
@@ -8,7 +8,51 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace First_MonoGame_Game
 {
+    // Author: Daniel Kowalski
+    // Purpose: Create a collectable Game Object that can sense when it's been collided with
+    // and can become inactive
+    // Restrictions: "Active" Property can only be set to false from outside this class
     class Collectable : GameObject
     {
+        // Fields
+        private bool active;
+
+        // Properties
+        public bool Active
+        {
+            get { return active; }
+
+            // There is never a situation where an inactive collectable should be reactivated
+            set { if (value == false) { active = value; } }
+        }
+
+        /// <summary>
+        /// Creates a new instance of a collectable game object
+        /// </summary>
+        public Collectable (Texture2D texture, int x, int y, int width, int height)
+            : base (texture, x, y, width, height)
+        {
+            active = true;
+        }
+
+        /// <summary>
+        /// Checks if this is an active collectable that is colliding with another game object
+        /// </summary>
+        /// <param name="check">The Game Object to check against</param>
+        /// <returns>True if colliding, false if not and false if the collectable is inactive</returns>
+        public bool CheckCollision (GameObject check)
+        {
+            if (!active) { return false; }
+            return base.Position.Intersects(check.Position);
+        }
+
+        /// <summary>
+        /// If this Collectable Game Object is active, draws it to the SpriteBatch
+        /// </summary>
+        /// <param name="sb">The SpriteBatch to draw to</param>
+        public override void Draw(SpriteBatch sb)
+        {
+            if (active) { base.Draw(sb); }
+        }
     }
 }
